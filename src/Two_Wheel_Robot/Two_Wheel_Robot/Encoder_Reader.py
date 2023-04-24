bd94bee93a924f1f0a14b5cf93e72ace01f59a57
Your Name <youremail@example.com>
2023-04-23 23:27:05 -0400

    asdf

--- src/Two_Wheel_Robot/Two_Wheel_Robot/Encoder_Reader.py
+++ src/Two_Wheel_Robot/Two_Wheel_Robot/Encoder_Reader.py
@@ -34,7 +34,8 @@ class Encoder_Reader(Node):
         line = self.chip.get_line(98)
         
         self.count=0
-        line.request(type=gpiod.line_request.DIRECTION_INPUT)
+        
+        line.request()
         self.encoder_pub = self.create_publisher(Int32, 'encoder_value', 10)
         
         self.timer_ = self.create_timer(0.1, self.read_encoder)
