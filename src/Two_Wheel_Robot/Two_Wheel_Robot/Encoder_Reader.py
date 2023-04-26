a47e34aee9c5ffb16b63b629243c811be39f5999
Your Name <youremail@example.com>
2023-04-25 20:34:40 -0400

    removed motor 2

--- src/Two_Wheel_Robot/Two_Wheel_Robot/Encoder_Reader.py
+++ src/Two_Wheel_Robot/Two_Wheel_Robot/Encoder_Reader.py
@@ -32,42 +32,42 @@ class Encoder_Reader(Node):
 
         M1E2=gpiod.chip(0)#Motor 1, Encoder 2, Pin 13 = chip 0, line 9, (Yellow Wire)
         M1E1 = gpiod.chip(1)#Motor 1, Encoder 1, Pin 7 = chip 1, line 98, (Green Wire)
-        M2E1=gpiod.chip(0)#Motor 2, Encoder 1, Pin 15  = chip 0, line 10 (Green Wire)
-        M2E2 = gpiod.chip(1)#Motor 2, Encoder 2, Pin 16  = chip 1, line 93 (Yellow Wire)
+        #M2E1=gpiod.chip(0)#Motor 2, Encoder 1, Pin 15  = chip 0, line 10 (Green Wire)
+        #M2E2 = gpiod.chip(1)#Motor 2, Encoder 2, Pin 16  = chip 1, line 93 (Yellow Wire)
         
         self.M1E1_Line = M1E1.get_line(98)
         self.M1E2_Line=M1E2.get_line(9)
-        self.M2E1_Line = M2E1.get_line(10)
-        self.M2E2_Line=M2E2.get_line(93)
+        #self.M2E1_Line = M2E1.get_line(10)
+        #self.M2E2_Line=M2E2.get_line(93)
         
         self.count=0
 
         M1E1_config=gpiod.line_request()
         M1E2_config=gpiod.line_request()
-        M2E1_config=gpiod.line_request()
-        M2E2_config=gpiod.line_request()
+        #M2E1_config=gpiod.line_request()
+        #M2E2_config=gpiod.line_request()
 
         M1E1_config.consumer="Encoder_Reader"
         M1E2_config.consumer="Encoder_Reader"
-        M2E1_config.consumer="Encoder_Reader"
-        M2E2_config.consumer="Encoder_Reader"
+        #M2E1_config.consumer="Encoder_Reader"
+        #M2E2_config.consumer="Encoder_Reader"
 
         M1E1_config.request_type=gpiod.line_request.DIRECTION_INPUT
         M1E2_config.request_type=gpiod.line_request.DIRECTION_INPUT
-        M2E1_config.request_type=gpiod.line_request.DIRECTION_INPUT
-        M2E2_config.request_type=gpiod.line_request.DIRECTION_INPUT
+        #M2E1_config.request_type=gpiod.line_request.DIRECTION_INPUT
+        #M2E2_config.request_type=gpiod.line_request.DIRECTION_INPUT
 
         self.M1E1_Line.request(M1E1_config)
         self.M1E2_Line.request(M1E2_config)
-        self.M2E1_Line.request(M2E1_config)
-        self.M2E2_Line.request(M2E2_config)
+        #self.M2E1_Line.request(M2E1_config)
+        #self.M2E2_Line.request(M2E2_config)
 
         self.encoder_pub = self.create_publisher(Int32MultiArray, 'encoder_value', 10)
         self.timer_ = self.create_timer(0.1, self.read_encoder)
 
     def read_encoder(self):
         value=Int32MultiArray
-        value=[self.M1E1_Line.get_value(), self.M1E2_Line.get_value(), self.M2E1_Line.get_value(), self.M2E2_Line.get_value()]
+        value=[self.M1E1_Line.get_value(), self.M1E2_Line.get_value()]#, self.M2E1_Line.get_value(), self.M2E2_Line.get_value()]
         self.encoder_pub.publish(Int32MultiArray(data=value))
 
     #def __del__(self):
